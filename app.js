8ec653c1ce78a035f097ae4f591c49bc52988210
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-02-07 13:57:36 -0800

    changed js, html, and added style

--- app.js
+++ app.js
@@ -9,19 +9,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // optionally update when fields update
     // tbd
+    document.getElementById(`name`).addEventListener(`change`, function (eventData) {
+      let nameOutput = document.getElementById(`nameOutput`);
+      nameOutput.innerText = eventData.target.value;
+      console.log(eventData);
+    })
 
+    
     // process form Data
-    document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault();
-      
-        var formData = new FormData(form);
-        // output as an object
-        console.log(Object.fromEntries(formData));
-      
-        // ...or iterate through the name-value pairs
-        for (var pair of formData.entries()) {
-          console.log(pair[0] + ": " + pair[1]);
-        }
+    document.getElementById("myForm").addEventListener("submit", function (eventData) {
+        eventData.preventDefault();
+        //stop page reload when form is submitted
+        console.log(eventData.target);
+        var formData = new FormData(eventData.target);
+        formData = Object.fromEntries(formData);
+        
+        //update certificate info when form is submitted
+        let nameOutput = document.getElementById(`nameOutput`);
+        let info = document.getElementById(`info`);
+        nameOutput.innerText = formData.name;
+        info.innerText = formData.type;
       });
     // Log readiness to console
     console.log("Ready");
